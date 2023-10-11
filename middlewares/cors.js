55dd83bb051af7587f7205822ef03f14ca8b5275
Alexandra Kuular <sakuular@gmail.com>
2023-10-12 00:18:05 +0300

    CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,6 @@
 const allowedCors = [
+  'https://api.movies.alexkuular.nomoredomainsicu.ru',
+  'http://api.movies.alexkuular.nomoredomainsicu.ru',
   'https://api.movies.alexkuular.nomoredomainsrocks.ru',
   'http://api.movies.alexkuular.nomoredomainsrocks.ru',
   'http://localhost:3000',
